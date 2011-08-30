79ffb7346d9adfafcba197d69b55d67b0678292c
Jon Miller <jonEbird@gmail.com>
2011-08-29 20:03:39 -0400

    Changing my mind about sending email messages from cmdline. Use stdin for body

--- predictions.py
+++ predictions.py
@@ -546,8 +546,11 @@ if __name__ == "__main__":
         sms_message(sys.argv[2])
         sys.exit(0)
 
-    elif (len(sys.argv) == 4 and sys.argv[1] in ['email_all']):
-        email_message(sys.argv[2], sys.argv[3])
+    elif (len(sys.argv) == 3 and sys.argv[1] in ['email_all']):
+        body = sys.stdin.read()
+        print 'You want to send "%s"?' % body
+        sys.exit(0)
+        email_message(sys.argv[2])
         sys.exit(0)
 
     #web.run(urls, globals())
