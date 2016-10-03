e638db6ea64235945d7a6d1edd19fe8f73f6e93b
Jon Miller <jonmiller@apple.com>
2016-10-02 23:31:01 -0700

    deploy.sh: Fix deployment for production
    This script is an embarrassment but need to fix the minimum to get it
    usable again when trying to push changes.

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-cd $(dirname $0)
+set -e 
+
+cd "$(dirname "$0")"
 SRC_DIR=$(pwd)
 DST_DIR="/var/www/buckeyepredictions"
 
-if [ "$SRC_DIR" == "$DST_DIR" ]; then
-    echo "Error: Do not run this from the deployment target directory. Exiting."
-    exit 2
-fi
-
-if [ ! -d ${DST_DIR} ]; then
-    echo "Error: You are not able to push to production: \"${DST_DIR}\" not found to push to. Exiting."
-    exit 1
-fi
-
 usage() {
     cat <<EOF
-Usage: $(basename -- $0) [-n] [pulldb]
+Usage: $(basename -- $0) [-n] [pulldb] [prod]
+       If "prod" not specified, will try to deploy locally.
 EOF
 }
 
 TESTING="no"
+MODE="dev"
 [ "$1" == '-n' ] && { shift; TESTING="yes"; }
 
 # Other ways to use this script
 # 1. Pull production DB
-if [ "$1" == "--help" -o "$1" == "-h" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     usage
     exit 0
 elif [ "$1" == "pulldb" ]; then
@@ -33,6 +27,7 @@ elif [ "$1" == "pulldb" ]; then
     ssh linbird "cat /var/www/buckeyepredictions/predictions/predictions.sqlite" | sudo -u apache tee /var/www/buckeyepredictions/predictions/predictions.sqlite >/dev/null
     exit 0
 elif [ "$1" == "prod" ]; then
+    MODE="prod"
     # git archive --format=tar.gz HEAD | ssh linbird "tar -C /var/www/buckeyepredictions -xzvf -"
     git ls-files | \
         xargs ls 2>&- | \
@@ -40,6 +35,17 @@ elif [ "$1" == "prod" ]; then
         ssh linbird "tar -C /var/www/buckeyepredictions -xvf -"
     ssh linbird "service httpd restart"
     exit 0
+fi
+
+# Sanity checks
+if [ "$SRC_DIR" == "$DST_DIR" ]; then
+    echo "Error: Do not run this from the deployment target directory. Exiting."
+    exit 1
+fi
+
+if [ $MODE == "dev" ] && [ ! -d ${DST_DIR} ]; then
+    echo "Error: You are not able to push to production: \"${DST_DIR}\" not found to push to. Exiting."
+    exit 2
 fi
 
 git ls-files |\
